057fc398dd6aaaaaba19dd3d33899d2677161070
Loki <wangjian1009@gmail.com>
2018-04-12 11:41:33 +0800

    补充kcp发送代码的函数

--- PacketTunnel/KcpSocket.swift
+++ PacketTunnel/KcpSocket.swift
@@ -1,8 +1,29 @@
 import Foundation
 import NEKit
+import CocoaLumberjackSwift
 
 public class KcpSocket: NSObject, RawTCPSocketProtocol {
+    var kcp: UnsafeMutablePointer<ikcpcb>?
+    
     weak open var delegate: RawTCPSocketDelegate?
+
+    private static var conv = 1
+    public override init() {
+        kcp = nil;
+        super.init()
+
+        let holder = Unmanaged.passRetained(self)
+        let pointer = UnsafeMutableRawPointer(holder.toOpaque())
+        kcp = ikcp_create(UInt32(KcpSocket.conv), pointer)
+        KcpSocket.conv+=1
+        DDLogError("kcp[\(ikcp_getconv(kcp))]: create")
+    }
+
+    deinit {
+        DDLogError("kcp[\(ikcp_getconv(kcp))]: release")
+        ikcp_release(kcp)
+        kcp = nil;
+    }
 
     /// If the socket is connected.
     open var isConnected: Bool {
@@ -25,6 +46,11 @@ public class KcpSocket: NSObject, RawTCPSocketProtocol {
     }
 
     public func write(data: Data) {
+        let rv = data.withUnsafeBytes { (d: UnsafePointer<CChar>) -> CInt in
+            return ikcp_send(kcp, d, Int32(data.count))
+        }
+        
+        DDLogError("kcp ==> write\(data.count), rv=\(rv)")        
     }
 
     public func readData() {
